c171521f5617f6f7d2c646b628d7f01ebe55b0fb
Joe Wroe <joe.wroe@dft.gov.uk>
2021-08-22 22:59:24 +0100

    Add Post routes
    JW

---
+++ src/main/kotlin/blog/wroejoe/routes/PostRoutes.kt
@@ -0,0 +1,20 @@
+package blog.wroejoe.routes
+
+import io.ktor.routing.*
+
+fun Route.postRouting() {
+    route("/post") {
+        get {
+
+        }
+        get("{id}") {
+
+        }
+        post {
+
+        }
+        delete("{id}") {
+            
+        }
+    }
+}
